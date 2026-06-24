e071c9a4f8b9abfc485d5f56a19a00b9c839b07e
prashant17d97 <prashantsinghsca@gmail.com>
2026-06-24 22:56:40 +0530

    chore: remove unused promo store

--- src/store/promo.store.ts
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
-
-interface PromoState { code: string | null; setCode: (code: string | null) => void; }
-
-export const usePromoStore = create<PromoState>()(
-  persist((set) => ({ code: null, setCode: (code) => set({ code }) }), { name: "cd:promo" }),
-);
